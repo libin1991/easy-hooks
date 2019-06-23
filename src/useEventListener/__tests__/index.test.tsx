c9fb0e547478cc5672bbe5749629c8cedb5464ee
libin1991 <3120217729@qq.com>
2019-06-23 18:51:30 +0800

    add more js

---
+++ src/useEventListener/__tests__/index.test.tsx
@@ -0,0 +1,9 @@
+import { cleanup } from 'react-hooks-testing-library'
+
+describe('useEventListener test', () => {
+  afterEach(cleanup)
+
+  it('should use eventListener', () => {
+
+  })
+})
---
+++ src/useImage/__tests__/index.test.tsx
@@ -0,0 +1,9 @@
+import { cleanup } from 'react-hooks-testing-library'
+
+describe('use image test', () => {
+  afterEach(cleanup)
+
+  it('should use image', () => {
+    
+  })
+})
